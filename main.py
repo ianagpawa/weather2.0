e74ce990d915ede8ee9da5438f32648a60fb988f
Ian Agpawa <agpawaji@gmail.com>
2017-06-04 13:12:30 -0400

    feat: Start api

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
+import os
 import webapp2
+import json
+import httplib2
+import requests
+
 from Handler import Handler
 
 class MainPage(Handler):
@@ -7,6 +12,19 @@ class MainPage(Handler):
     This class is a child of Handler and is for FrontPage.
     """
     def get(self):
+        ip = self.request.remote_addr
+        KEY = json.loads(open('client_secrets.json', 'r').read())['UNDERGROUND']
+        url = "http://api.wunderground.com/api/%s/forecast/q/autoip.json?geo_ip=%s.json" % (KEY, ip)
+
+        h = httplib2.Http()
+        result = json.loads(h.request(url,'GET')[1])
+
+        if result.get('error') is not None:
+            response = make_response(json.dumps(result.get('error')), 500)
+            response.heads['Content-Type'] = 'application/json'
+            print response
+            return response
+        print result
 
         return self.render("main_page.html")
 
