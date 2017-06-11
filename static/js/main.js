84a2be27f8c60ed9fa67ab4f0bc113917029720b
Ian Agpawa <agpawaji@gmail.com>
2017-06-10 22:39:31 -0400

    feat: Start modal for days

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-function displayAll(){
+function conversion(){
     metric = false;
     $("#convert-button").on('click', function(){
         if (metric == false){
@@ -17,6 +17,23 @@ function displayAll(){
     });
 }
 
+function modal(){
+    $('#Modal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('obj') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+    })
+}
+
+
+function displayAll() {
+    conversion();
+    modal();
+}
 
 
 $(document).ready(displayAll)
