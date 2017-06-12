57ff9cad3999b3508a8a9ba4f1b3de5953e75988
Ian Agpawa <agpawaji@gmail.com>
2017-06-12 11:12:38 -0400

    feat: Add weather day obj to modal, update jquery for weather day obj values

--- static/js/main.js
+++ static/js/main.js
@@ -20,16 +20,30 @@ function conversion(){
 function modal(){
     $('#Modal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var day = button.data('day') // Extract info from data-* attributes
+        // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var day = button.data('day')
         var monthname = button.data('monthname')
+        var year = button.data('year')
+        var weekday = button.data('weekday')
+        var location = button.data('location')
+        var high = button.data('high')
+        var low = button.data('low')
+        var icon = button.data('icon')
+        var iconurl = button.data('iconurl')
+        var conditions = button.data('conditions')
+        var humidity = button.data('humidity')
+        var windDirection = button.data('wind-direction')
+        var windStrength = button.data('wind-strength')
+        var text = button.data('text')
+
         var modal = $(this)
         // modal.find('.modal-title').text(day)
         // modal.find('.modal-body input').val(day)
 
         // console.log(JSON.parse(day))
-        console.log(monthname)
+        console.log([day, monthname, year, weekday, location, high, low, icon,iconurl, conditions, humidity, windDirection, windStrength, text])
         // modal.find(".modal-content")
     })
 }
