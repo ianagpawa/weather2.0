c70b7cf0098532147c3ceca1c707900b2a189d03
Ian Agpawa <agpawaji@gmail.com>
2017-06-26 22:56:01 -0400

    refactor: Start change to Open Weather API

--- Weather.py
+++ Weather.py
@@ -1,55 +1,63 @@
 import pprint
 def get_weather(result):
-    arr = []
+    main = result['list']
+    for reading in main:
+        thing = reading['dt_txt']
+        temp = reading['main']['temp']
 
-    for i in range(7):
-        simpleforecast = result['forecast']['simpleforecast']['forecastday'][i]
-        txtforecast = result['forecast']['txt_forecast']['forecastday'][i]
-
-        date = simpleforecast['date']
-
-        day = date['day']
-        monthname = date['monthname']
-        year = date['year']
-        weekday = date['weekday']
-        tz_long = date['tz_long']
-        city_name = tz_long.split("/")[1]
-        location = " ".join(city_name.split("_"))
-
-        high = simpleforecast['high']
-        # {u'celsius': u'19', u'fahrenheit': u'66'}
-
-        low = simpleforecast['low']
-        # {u'celsius': u'16', u'fahrenheit': u'61'}
-
-        icon = simpleforecast['icon']
-        icon_url = simpleforecast['icon_url']
-        conditions = simpleforecast['conditions']
-
-        humidity = simpleforecast['avehumidity']
-        wind = simpleforecast['avewind']
-        direction = wind['dir']
-        mph = wind['mph']
-        kph = wind['kph']
-
-        fcttext = txtforecast['fcttext']
-        fcttext_metric = txtforecast['fcttext_metric']
+        print "On %s it is %s" % (thing, temp)
 
 
-        current = {
-            "day": day,
-            "monthname": monthname,
-            "year": year,
-            "weekday": weekday,
-            "location": location,
-            "high": high,
-            "low": low,
-            "icon": icon,
-            "icon_url": icon_url,
-            "conditions": conditions,
-            "humidity": humidity,
-            "wind": [direction, [mph, kph]],
-            "text": [fcttext, fcttext_metric]
-            }
-        arr.append(current)
-    return arr
+
+    # arr = []
+    #
+    # for i in range(7):
+    #     simpleforecast = result['forecast']['simpleforecast']['forecastday'][i]
+    #     txtforecast = result['forecast']['txt_forecast']['forecastday'][i]
+    #
+    #     date = simpleforecast['date']
+    #
+    #     day = date['day']
+    #     monthname = date['monthname']
+    #     year = date['year']
+    #     weekday = date['weekday']
+    #     tz_long = date['tz_long']
+    #     city_name = tz_long.split("/")[1]
+    #     location = " ".join(city_name.split("_"))
+    #
+    #     high = simpleforecast['high']
+    #     # {u'celsius': u'19', u'fahrenheit': u'66'}
+    #
+    #     low = simpleforecast['low']
+    #     # {u'celsius': u'16', u'fahrenheit': u'61'}
+    #
+    #     icon = simpleforecast['icon']
+    #     icon_url = simpleforecast['icon_url']
+    #     conditions = simpleforecast['conditions']
+    #
+    #     humidity = simpleforecast['avehumidity']
+    #     wind = simpleforecast['avewind']
+    #     direction = wind['dir']
+    #     mph = wind['mph']
+    #     kph = wind['kph']
+    #
+    #     fcttext = txtforecast['fcttext']
+    #     fcttext_metric = txtforecast['fcttext_metric']
+    #
+    #
+    #     current = {
+    #         "day": day,
+    #         "monthname": monthname,
+    #         "year": year,
+    #         "weekday": weekday,
+    #         "location": location,
+    #         "high": high,
+    #         "low": low,
+    #         "icon": icon,
+    #         "icon_url": icon_url,
+    #         "conditions": conditions,
+    #         "humidity": humidity,
+    #         "wind": [direction, [mph, kph]],
+    #         "text": [fcttext, fcttext_metric]
+    #         }
+    #     arr.append(current)
--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import requests
 import pprint
 from Weather import get_weather
 
+import urllib, json
+
 from Handler import Handler
+import pprint
 
 
 class MainPage(Handler):
@@ -15,27 +18,50 @@ class MainPage(Handler):
     This class is a child of Handler and is for FrontPage.
     """
     def get(self):
-        ip = self.request.remote_addr
-        KEY = json.loads(open('client_secrets.json', 'r').read())['UNDERGROUND']
-        TEST_IP = json.loads(open('client_secrets.json', 'r').read())['TEST_IP']
+
+        # KEY = json.loads(open('client_secrets.json', 'r').read())['UNDERGROUND']
         # NEED TO CHANGE IP
-        url = "http://api.wunderground.com/api/%s/forecast10day/q/autoip.json?geo_ip=%s" % (KEY, ip)
+        # url = "http://api.wunderground.com/api/%s/forecast10day/q/autoip.json?geo_ip=%s" % (KEY, ip)
 
-        h = httplib2.Http()
-        result = json.loads(h.request(url,'GET')[1])
+        ip = self.request.remote_addr
+        TEST_IP = json.loads(open('client_secrets.json', 'r').read())['TEST_IP']
 
-        if result.get('error') is not None:
-            response = make_response(json.dumps(result.get('error')), 500)
-            response.heads['Content-Type'] = 'application/json'
-            return response
+        location_get_url = "http://ip-api.com/json/" + TEST_IP
+        response = urllib.urlopen(location_get_url)
+        data = json.loads(response.read())
+        longitude, latitude, city = data['lon'], data['lat'], data['city']
 
-        weather_days = get_weather(result)
-        today = weather_days[0]
-        rest = weather_days[1:]
+        KEY = json.loads(open('client_secrets.json', 'r').read())['OPENWEATHER']
+        url = "http://api.openweathermap.org/data/2.5/forecast?lat=%s&lon=%s&APPID=%s" % (latitude, longitude, KEY)
+
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+
+        thing = get_weather(data)
+        pprint.pprint(thing)
+        # thing = pprint.pprint(data)
 
 
+        # h = httplib2.Http()
+        # result = json.loads(h.request(url,'GET'))
 
-        return self.render("main_page.html", today=today, rest=rest)
+        # print result
+        #
+        # if result.get('error') is not None:
+        #     response = make_response(json.dumps(result.get('error')), 500)
+        #     response.heads['Content-Type'] = 'application/json'
+        #     return response
+        #
+        # weather_days = get_weather(result)
+        # today = weather_days[0]
+        # rest = weather_days[1:]
+        #
+        #
+        today = {}
+        rest = {}
+
+        return self.render("main_page_revamp.html", today=today, rest=rest)
+        # return self.write(thing)
 
 
 
