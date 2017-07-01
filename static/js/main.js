f28d477fbea4c81ff86c61879f3d922e45bdf2bd
Ian Agpawa <agpawaji@gmail.com>
2017-06-30 23:39:57 -0400

    refactor: Update metric and imperial for hourly, update js to change display to initial when clicked

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,13 @@ function conversion(){
     $("#convert-button").on('click', function(){
         if (metric == false){
             $(".imperial").css('display', 'none')
-            $('.metric').css('display', 'block')
+            $('.metric').css('display', 'initial')
             $("#convert-button").html('IMPERIAL')
             $("#convert-button").css("background-color", "#000639")
             metric = true
         } else {
             $(".metric").css('display', 'none')
-            $('.imperial').css('display', 'block')
+            $('.imperial').css('display', 'initial')
             $("#convert-button").html('METRIC')
             $("#convert-button").css("background-color", "#660000")
             metric = false
