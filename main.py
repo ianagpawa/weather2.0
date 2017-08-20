12383e3a32888ed4157b3198f95ac7c52b83e724
Ian Agpawa <agpawaji@gmail.com>
2017-08-20 14:34:49 -0400

    feat: Start add search for city

--- main.py
+++ main.py
@@ -23,13 +23,15 @@ class MainPage(Handler):
         # ip = self.request.remote_addr
 
         # USING GEO COORDINATES
-        # TEST_GEO = json.loads(open('client_secrets.json', 'r').read())['TEST_GEO']
-        geo = self.request.headers['X-Appengine-CityLatLong']
+        geo = json.loads(open('client_secrets.json', 'r').read())['TEST_GEO']
+
+        # geo = self.request.headers['X-Appengine-CityLatLong']
 
 
         hourly_url = "http://api.wunderground.com/api/%s/hourly10day/q/%s.json" % (KEY, geo)
         h = httplib2.Http()
         hourly_result = json.loads(h.request(hourly_url,'GET')[1])
+        pprint.pprint(hourly_result)
 
         if hourly_result.get('error') is not None:
             response = make_response(json.dumps(hourly_result.get('error')), 500)
@@ -50,6 +52,41 @@ class MainPage(Handler):
         today, rest = get_all_weather(hourly_result, forecast_result)
 
         return self.render("main_page.html", today=today, rest=rest)
+
+    def post(self):
+        if self.request.get("city"):
+            city_name = self.request.get('city')
+            city_name = "_".join(city_name.split(" "))
+
+            KEY = json.loads(open('client_secrets.json', 'r').read())['UNDERGROUND']
+
+
+            hourly_url = "http://api.wunderground.com/api/%s/hourly10day/q/%s.json" % (KEY, city_name)
+            h = httplib2.Http()
+            hourly_result = json.loads(h.request(hourly_url,'GET')[1])
+
+            if hourly_result.get('error') is not None:
+                response = make_response(json.dumps(hourly_result.get('error')), 500)
+                response.heads['Content-Type'] = 'application/json'
+                return response
+
+
+            forecast_url = "http://api.wunderground.com/api/%s/forecast10day/q/%s.json" % (KEY, city_name)
+            j = httplib2.Http()
+            forecast_result = json.loads(j.request(forecast_url,'GET')[1])
+
+
+
+            if forecast_result.get('error') is not None:
+                response = make_response(json.dumps(forecast_result.get('error')), 500)
+                response.heads['Content-Type'] = 'application/json'
+                return response
+
+
+            today, rest = get_all_weather(hourly_result, forecast_result)
+
+            return self.render("main_page.html", today=today, rest=rest)
+
 #
 # class TestPage(Handler):
 #         def get(self):
@@ -63,5 +100,5 @@ class MainPage(Handler):
 
 
 app = webapp2.WSGIApplication([ ("/", MainPage)
-                                ],
-                                debug=True)
+                                ])
+                                # debug=True)
